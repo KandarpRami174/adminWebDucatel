1536e60c12956355363ca14501ee7e027a38b828
Kandarp <kandarp099@gmail.com>
2020-08-09 18:09:19 +0530

    moment pck init

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import { routes } from './router/routes'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import { routes } from "./router/routes";
+import moment from "moment";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
+Vue.filter("formatDate", function(value) {
+  if (value) {
+    return moment(String(value)).format("MMMM DD, YYYY");
+  }
+});
+
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
-})
+  mode: "history",
+  routes,
+});
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
