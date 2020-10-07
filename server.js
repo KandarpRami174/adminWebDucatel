55b9120a393406882124902c42c6307272dd5369
Kandarp Rami <rozer@Kandarps-MacBook-Air.local>
2020-10-07 09:53:21 +0530

    Minor Bug fix

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+// const serveStatic = require('serve-static')
+// const path = require('path')
 const port = process.env.PORT || 1058;
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))
+// app.use('/', serveStatic(path.join(__dirname,'/dist')))
+app.use(express.static(__dirname,'/dist'))
 app.get(/.*/, function(req, res) {
     res.sendFile(__dirname + "/dist/index.html");
 });
 
 app.listen(port)
 
-console.log('Post Listen: ' + port)
+console.log('Server Start w/ Post Listen: ' + port)
