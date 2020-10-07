b788b62e21169b06c3439a10a38fc699268cf30f
Kandarp Rami <rozer@Kandarps-MacBook-Air.local>
2020-10-07 12:05:34 +0530

    vue size update

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,21 @@ module.exports = {
   },
   configureWebpack: {
     devtool: "source-map",
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
   },
-  chainWebpack: config => {
-    config.performance
-      .maxEntrypointSize(400000)
-      .maxAssetSize(400000)
-  },
+  // chainWebpack: config => {
+  //   config.performance
+  //     .maxEntrypointSize(400000)
+  //     .maxAssetSize(400000)
+  // },
   publicPath:
     process.env.NODE_ENV === "production"
       ? "/" + process.env.CI_PROJECT_NAME + "/"
