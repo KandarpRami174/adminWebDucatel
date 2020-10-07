d23a169b8dd92e22d383ac066e600a92593f6317
Kandarp Rami <rozer@Kandarps-MacBook-Air.local>
2020-10-07 11:25:10 +0530

    dist bug fix

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express')
-// const serveStatic = require('serve-static')
-// const path = require('path')
 const port = process.env.PORT || 1058;
-
 const app = express()
 
-// app.use('/', serveStatic(path.join(__dirname,'/dist')))
 app.use(express.static(__dirname,'/dist'))
 app.get(/.*/, function(req, res) {
     res.sendFile(__dirname + "/dist/index.html");
